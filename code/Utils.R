2ec9310823439e5383f12e6b84caec4ecf4c5681
Hair Parra <jair.parra@outlook.com>
2023-04-10 13:23:45 -0800

    updated Copula with Two risk drivers

--- code/Utils.R
+++ code/Utils.R
@@ -51,6 +51,10 @@ initialize_sim_mats <- function(sim_mat, lnames=NULL, num_mats=3){
   return(mats)
 }
 
+#######################################################
+### 2. Price prediction from Simulated Log-returns ####
+#######################################################
+
 
 f_next_Pt <- function(Pt, log_Rt_next){
   #### Wrapper for price_option for two vectors of prices and volatilities 
@@ -73,10 +77,15 @@ f_next_Pt <- function(Pt, log_Rt_next){
   return(Pt_next)
 }
 
-# Expected shortfall implementation 
-es <- function(returns,  alpha = 0.05) {
-  ### Implementation of expected shortfall 
-  var <- quantile(returns, alpha)
-  es <- mean(returns[returns < var])
-  es
+#######################
+### 3. VaR and ES ####
+######################
+
+f_VaR_ES <- function(x, alpha=0.05) {
+  ### Calculates the VaR and the  ES at alpha level 
+  
+  var <- as.numeric(quantile(x, probs = alpha))
+  es <- mean(x[x< var])
+  
+  return(list(VaR=var, ES=es))
 }
